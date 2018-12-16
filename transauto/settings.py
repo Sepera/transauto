4e99a59c773a34972302f018b6702a9df48a00ae
Sergey Kravchenko <ks-spb@mail.ru>
2018-12-16 21:15:12 +0300

    2nd commit

--- transauto/settings.py
+++ transauto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(9rmrq(xb3qnnu8z_nuw4fwbhd62em+uh_ljjus*1-bgbin9r+'
+SECRET_KEY = 'KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
